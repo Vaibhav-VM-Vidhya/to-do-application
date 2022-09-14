e40711da34890a207bf9d294ebfab3f68ec810ba
vaibhav-vm-vidhya <vaibhavvakil1234@gmail.com>
2022-09-14 23:33:50 +0530

    bugs removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import Mainpage from './Components/Mainpage';
-import Addtask from './Components/Tasks/Addtask';
+
 import Taskstate from './Context/Notes/Taskstate'
 function App() {
   return (
   <Taskstate>
   <div className='bg-gradient-to-r from-red-300 scroll-smooth relative to-cyan-300 h-screen w-screen overflow-scroll'>
-    <Addtask/>
+   
     <Mainpage/>
     
     </div>
